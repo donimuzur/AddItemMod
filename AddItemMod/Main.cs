fb4415d22d9f346934c25f12c4cd071f7f4d7dce
Muhammad Zulfi Rusdani <donimuzur@gmail.com>
2022-08-28 22:14:36 +0700

    create button AddItemMenu

--- AddItemMod/Main.cs
+++ AddItemMod/Main.cs
@@ -1,5 +1,9 @@
 ﻿using MelonLoader;
+using System;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
+
 namespace AddItemMod
 {
 
@@ -9,13 +13,16 @@ namespace AddItemMod
         public const string Description = "Mod to Add Item";
         public const string Author = "donimuzur"; // Author of the Mod.  (MUST BE SET)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.1.1"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "2.0.0"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)    
     }
 
     public class AddItemMod : MelonMod
     {
- 
+
+        private GameManager component = null;
+        private GameObject obj = null;
+        private GameObject buttonTemplate = null;
         public override void OnApplicationStart()
         {
             MelonLogger.Msg("AddItemMod Started");
@@ -190,6 +197,66 @@ namespace AddItemMod
                         getBuilding.storage.AddItems(itemBundle);
                     }
                 }
+            }
+
+            if (obj != null)
+            {
+                return;
+            }
+            var Button_Resume = GameObject.Find("Button_Resume");
+            if (Button_Resume != null)
+            {
+                buttonTemplate = GameObject.Instantiate(Button_Resume);
+            }
+            var getTowncenter = GameObject.FindObjectOfType<UIBuildingInfoWindowStorageModuleCollapsible>();
+            if (getTowncenter != null)
+            {
+                MelonLogger.Msg("Creating UI AddItemButton");
+                
+                obj = getTowncenter.gameObject;
+            }
+            if (obj != null && buttonTemplate != null)
+            {
+            
+                buttonTemplate.transform.DetachChildren();
+                buttonTemplate.name = "AddItemButton";
+
+                var buttonAction = buttonTemplate.GetComponent<Button>();
+                RectTransform toDestroy1 = buttonTemplate.GetComponent<RectTransform>();
+                GameObject.Destroy(toDestroy1);
+
+                var toDestroy2 = buttonTemplate.GetComponent<LayoutElement>();
+                GameObject.Destroy(toDestroy2);
+
+                buttonTemplate.AddComponent<RectTransform>();
+
+                var buttonTemplatelayoutElement = buttonTemplate.AddComponent<LayoutElement>();
+                buttonTemplatelayoutElement.ignoreLayout = true;
+
+                GameObject gameObject2 = new GameObject("AddItemButtonText");
+                var component21 = gameObject2.AddComponent<RectTransform>();
+
+                var component22 = gameObject2.AddComponent<Text>();
+                component22.text = "Open Menu";
+                component22.fontSize = 14;
+                component22.fontStyle = FontStyle.Bold;
+                component22.color = Color.white;
+                component22.AssignDefaultFont();
+                component22.alignment = TextAnchor.MiddleCenter;
+
+                gameObject2.transform.SetParent(buttonTemplate.transform, false);
+
+                var component3 = buttonTemplate.AddComponent<HorizontalLayoutGroup>();
+                component3.childAlignment = TextAnchor.MiddleCenter;
+
+                var buttonTemplateRectTransofrm = buttonTemplate.GetComponent<RectTransform>();
+                buttonTemplateRectTransofrm.sizeDelta = new Vector2(150, 25);
+
+                buttonTemplate.transform.SetParent(obj.transform, false);
+                
+                buttonTemplate.transform.localPosition = new Vector3(150, 0, 0);
+
+                buttonTemplate.SetActiveRecursively(true);
             }
         }
     }
